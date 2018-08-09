e32e2e1aa126868ae5bdbdb6ef32b6abb314403b
Shantanu Kumar <shantanu0412kumar@gmail.com>
2018-08-09 12:41:01 +0530

    first

---
+++ api/models/users.js
@@ -0,0 +1,55 @@
+var mongoose = require( 'mongoose' );
+var crypto = require('crypto');
+var jwt = require('jsonwebtoken');
+
+var userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  category: {
+    type:String,
+    required: true
+  },
+  phone: {
+    type:String,
+    unique: true,
+    required:true
+  },
+
+  hash: String,
+  salt: String
+});
+
+userSchema.methods.setPassword = function(password){
+  this.salt = crypto.randomBytes(16).toString('hex');
+  this.hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+};
+
+userSchema.methods.validPassword = function(password) {
+  var hash = crypto.pbkdf2Sync(password, this.salt, 1000, 64, 'sha512').toString('hex');
+  return this.hash === hash;
+};
+
+userSchema.methods.generateJwt = function() {
+  var expiry = new Date();
+  expiry.setDate(expiry.getDate() + 7);
+
+  return jwt.sign({
+    _id: this._id,
+    username: this.username,
+    exp: parseInt(expiry.getTime() / 1000),
+  }, "MY_SECRET"); // DO NOT KEEP YOUR SECRET IN THE CODE!
+};
+
+mongoose.model('User', userSchema);
